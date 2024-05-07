9396baf0af6f08a9e06fe4c0339b76c39e3e3c1e
Venkateswarlu Ganji <venki@errsole.com>
2024-05-07 18:25:05 +0530

    chore: add the Errsole Proxy Middleware for fastify example

---
+++ examples/proxy-middleware/fastify-custom-path.mjs
@@ -0,0 +1,31 @@
+// In this example, the Errsole Web Dashboard will be accessible at http://localhost:3000/errsole/logs/dashboard
+import Fastify from 'fastify';
+import expressPlugin from '@fastify/express';
+import errsole from 'errsole';
+import ErrsoleSequelize from 'errsole-sequelize';
+
+// Initialize Errsole
+errsole.initialize({
+  storage: new ErrsoleSequelize({
+    dialect: 'sqlite',
+    storage: '/tmp/logs.sqlite'
+  }),
+  path: '/logs/dashboard' // Custom path
+});
+
+const fastify = Fastify();
+await fastify.register(expressPlugin);
+
+// Register Errsole Proxy Middleware at the desired path (e.g., /errsole)
+// Make sure this is the first middleware used
+fastify.use('/errsole', errsole.fastifyProxyMiddleware());
+
+// Add other middlewares below the Errsole Proxy Middleware
+
+// Start the server
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
