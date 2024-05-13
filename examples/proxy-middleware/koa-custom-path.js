483bcaaa50fd8efc209d4153619abb425ec3ed3a
Venkateswarlu Ganji <venki@errsole.com>
2024-05-13 17:43:26 +0530

    docs: add koa middleware examle

---
+++ examples/proxy-middleware/koa-custom-path.js
@@ -0,0 +1,34 @@
+/*
+ * ─────────────────────────────────────────────────────────────────────
+ *                          ERRSOLE DASHBOARD SETUP
+ * ─────────────────────────────────────────────────────────────────────
+ * The Errsole Web Dashboard will be accessible at:
+ * http://localhost:3000/errsole/logs/dashboard
+ *
+ * Here's how the complete path is constructed:
+ * ─────────────────────────────────────────────────────────────────────
+ * Middleware base path:           '/errsole'
+ * Custom path in initialization:  '/logs/dashboard'
+ * ─────────────────────────────────────────────────────────────────────
+ */
+const errsole = require('errsole');
+const ErrsoleSequelize = require('errsole-sequelize');
+const Koa = require('koa');
+
+// Initialize Errsole with a custom path
+errsole.initialize({
+  storage: new ErrsoleSequelize({
+    dialect: 'sqlite',
+    storage: '/tmp/logs.sqlite'
+  }),
+  path: '/logs/dashboard' // Custom path
+});
+
+const app = new Koa();
+
+// Use Errsole Proxy Middleware
+app.use(errsole.koaProxyMiddleware('/errsole'));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
