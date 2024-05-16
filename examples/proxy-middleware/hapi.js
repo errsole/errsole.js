ed0cc24d59744048741507a8a7b19a7ed6ea7549
Venkateswarlu Ganji <venki@errsole.com>
2024-05-16 18:00:27 +0530

    docs: add hapi middleware examples

---
+++ examples/proxy-middleware/hapi.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const errsole = require('errsole');
+const ErrsoleSequelize = require('errsole-sequelize');
+
+// Insert the Errsole code snippet at the beginning of your app's main file
+errsole.initialize({
+  storage: new ErrsoleSequelize({
+    dialect: 'sqlite',
+    storage: '/tmp/logs.sqlite'
+  })
+});
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
+
+  // Register the Errsole Proxy Middleware at the desired path (e.g., /errsole)
+  // Make sure this is the first plugin to be registered
+  await server.register({
+    plugin: errsole.hapiProxyMiddleware('/errsole')
+  });
+
+  // Register other plugin below the Errsole Proxy Middleware
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
